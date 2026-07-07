8da0357c9debfc8563aab671d22ff993f00e3bae
Anonmiraj <ezzibrahimx@gmail.com>
2026-07-08 02:25:31 +0300

    fix: use electron_39 with permittedInsecurePackages

--- flake.nix
+++ flake.nix
@@ -9,8 +9,13 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
+
+      nixpkgs.config.permittedInsecurePackages = [
+        "electron-39.8.10"
+      ];
+
       perSystem = { pkgs, ... }: let
-        inherit (pkgs) lib stdenv bun makeWrapper symlinkJoin nodejs esbuild typescript runCommand electron;
+        inherit (pkgs) lib stdenv bun makeWrapper symlinkJoin nodejs esbuild typescript runCommand electron_39;
       in {
         packages.default = let
           version = "0.17.2-niri";
@@ -47,7 +52,7 @@
             nativeBuildInputs = [ makeWrapper ];
           } ''
             mkdir -p $out/bin
-            makeWrapper ${electron}/bin/electron $out/bin/SubMiner \
+            makeWrapper ${electron_39}/bin/electron $out/bin/SubMiner \
               --add-flags ${electronApp}/share/subminer \
               --add-flags "--no-sandbox" \
               --set-default ELECTRON_OZONE_PLATFORM_HINT "x11"
